7ee1cd4c4faf43001112a07136505214f65cfd07
Gaurav Mallya <gmallya@berkeley.edu>
2024-01-02 14:31:18 -0800

    Website restructure and update

---
+++ courses/main.js
@@ -0,0 +1,32 @@
+var images = [
+    "../files/guava1.png",
+    "../files/guava2.png",
+    "../files/guava3.png",
+    "../files/guava4.png",
+    "../files/guava5.png",
+    "../files/guava6.png",
+    "../files/guava7.png"
+];
+
+function randImg() {
+    var size = images.length
+    var y = Math.random()
+    var x
+    if (y <= .5) {
+        x = 0
+    }
+    else{
+        x = Math.floor(size * Math.random())
+    } 
+    document.getElementById('imagefile').src = images[x];
+}
+
+function randColors(){
+    elements = document.getElementsByClassName('sm')
+    for (let i = 0; i < elements.length; i++){
+        const randomColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+        elements[i].style.color = randomColor
+    }
+}
+
+randImg()
--- main.js
+++ main.js
@@ -3,7 +3,10 @@ var images = [
     "files/guava2.png",
     "files/guava3.png",
     "files/guava4.png",
-    "files/guava5.png"];
+    "files/guava5.png",
+    "files/guava6.png",
+    "files/guava7.png"
+];
 
 function randImg() {
     var size = images.length
@@ -12,10 +15,21 @@ function randImg() {
     if (y <= .5) {
         x = 0
     }
+    if (y > .5 && y <= .75) {
+        x = 5
+    }
     else{
         x = Math.floor(size * Math.random())
     } 
     document.getElementById('imagefile').src = images[x];
+}
+
+function randColors(){
+    elements = document.getElementsByClassName('sm')
+    for (let i = 0; i < elements.length; i++){
+        const randomColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+        elements[i].style.color = randomColor
+    }
 }
 
 randImg()
